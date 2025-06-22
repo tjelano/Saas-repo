3cf4113f53a4ac6701dfe38f1bd903dd343102ca
siluminate <tjelano1@live.nl>
2025-06-22 14:31:07 +0200

    chore: Codebase audit and cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* Required for pricing table */}
-      <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {/* Required for pricing table */}
+        <Script async src="https://js.stripe.com/v3/pricing-table.js"></Script>
+      </body>
     </html>
   );
 }
