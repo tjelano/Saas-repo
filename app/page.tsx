6f72b2c87aae998ca28a84d383f1da41310916bb
siluminate <tjelano1@live.nl>
2025-06-23 01:47:56 +0200

    Fix landing page layout by correcting container styles

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default async function Home() {
   ];
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center gap-24">
       <HeroSection user={user} />
       <HowItWorks />
       <AboutUsSection />
