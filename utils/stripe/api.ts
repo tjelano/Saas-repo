ce1c208d8b58bf82210755222ebe8a5675faea96
siluminate <tjelano1@live.nl>
2025-06-22 19:09:16 +0200

    fix(stripe): prevent crash on invalid subscription id

--- utils/stripe/api.ts
+++ utils/stripe/api.ts
@@ -5,8 +5,13 @@ import { eq } from "drizzle-orm";
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 const PUBLIC_URL = process.env.NEXT_PUBLIC_WEBSITE_URL ? process.env.NEXT_PUBLIC_WEBSITE_URL : "http://localhost:3000"
 export async function getStripePlan(email: string) {
-
     const user = await db.select().from(usersTable).where(eq(usersTable.email, email))
+    if (!user || user.length === 0) {
+        return "No Plan"
+    }
+    if (user[0].plan === 'none') {
+        return "No Plan"
+    }
     const subscription = await stripe.subscriptions.retrieve(
         user[0].plan
     );
